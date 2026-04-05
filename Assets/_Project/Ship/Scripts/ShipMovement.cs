76de83b7d932d7541c5f1bdbb17c703ebb82034d
Thanaloc <raphael.seguin.97@gmail.com>
2026-04-05 23:14:11 +0200

    add ship collision with islands

--- Assets/_Project/Ship/Scripts/ShipMovement.cs
+++ Assets/_Project/Ship/Scripts/ShipMovement.cs
@@ -1,3 +1,4 @@
+using Player;
 using UnityEngine;
 
 namespace Ship
@@ -9,13 +10,24 @@ namespace Ship
         [SerializeField] private float _DeccelRatio = .025f;
         [SerializeField] private float _RotationSpeed = 20f;
 
+        [Header("Collision")]
+        [SerializeField] private float _CollisionCheckRadius = 3f;
+        [SerializeField] private float _CollisionCheckDistance = 5f;
+
+        private LayerMask _islandLayerMask;
+
         private float _currentSpeed = 0;
         private float _aimedSpeed = 0;
         private float _wheelDirection = 0;
         private bool _isAnchorDown = false;
 
         public bool IsAnchorDown => _isAnchorDown;
-        
+
+        private void Start()
+        {
+            _islandLayerMask = LayerMask.GetMask("Island");
+        }
+
         private void Update()
         {
             if (!_isAnchorDown)
@@ -32,6 +44,14 @@ namespace Ship
             if (_wheelDirection != 0)
             {
                 transform.Rotate(0, _wheelDirection * _RotationSpeed * Time.deltaTime, 0);
+            }
+
+            RaycastHit hit;
+
+            Vector3 castOrigin = new Vector3(transform.position.x, 0f, transform.position.z);
+            if (Physics.SphereCast(castOrigin, _CollisionCheckRadius, transform.forward, out hit, _CollisionCheckDistance, _islandLayerMask))
+            {
+                _currentSpeed = 0;
             }
 
             transform.position += transform.forward * _currentSpeed * Time.deltaTime;
